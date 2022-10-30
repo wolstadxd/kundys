81c49ac95f863a85e94cc5b382bb40ac02a7ed85
wolstadxd <wolski.rd@gmail.com>
2022-10-30 20:53:12 +0200

    add swiper js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,48 @@
 // Custom Scripts
-// Custom scripts
-AOS.init();
+// // Custom scripts
+AOS.init({
+    offset: 80
+});
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.portfolio__arrow-right',
+        prevEl: '.portfolio__arrow-left',
+    },
+    loop: true,
+
+});
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const slides = document.querySelectorAll('.portfolio__projects-item'),
+//         prev = document.querySelector('.portfolio__slider-prev'),
+//         next = document.querySelector('.portfolio__slider-next')
+//
+//     let slideIndex = 0
+//
+//     next.addEventListener('click', () => {
+//         slideIndex++
+//         for (let i of slides) {
+//             if (slideIndex===0) {i.style.translate = '0'}
+//             if (slideIndex===1) {i.style.translate = slideIndex*-400 + 'px'}
+//             if (slideIndex===2) {i.style.translate = slideIndex*-400 + 'px'}
+//             if (slideIndex===3) {i.style.translate = slideIndex*-400 + 'px'}
+//             if (slideIndex>3) {i.style.translate = '0'
+//             slideIndex=0}
+//         }
+//     })
+//     prev.addEventListener('click', () => {
+//         slideIndex--
+//         for (let i of slides) {
+//             if (slideIndex===0) {i.style.translate = '0'}
+//             if (slideIndex===1) {i.style.translate = '-400px'}
+//             if (slideIndex===2) {i.style.translate = '-800px'}
+//             if (slideIndex===3) {i.style.translate = '-1200px'}
+//             if (slideIndex<0) {slideIndex=0}
+//         }
+//     })
+// })
 ;
 
